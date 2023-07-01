799dec9da1a97a51cd6ca4172bbf4acbc4570959
vitoriacurty <vitoria.curty@hotmail.com>
2023-07-01 16:31:49 -0300

    implementação do requisito POST/participants

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+import Joi from "joi"
+import dayjs from "dayjs"
 
 //Criação do app
 const app = express()
@@ -8,15 +11,62 @@ const app = express()
 //Configurações
 app.use(cors())
 app.use(express.json())
+dotenv.config()
 
 //Conexão com o Banco
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
-let db 
+let db
 
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
+const promise = mongoClient.connect()
+promise.then(() => db = mongoClient.db())
+promise.catch((err) => console.log(err.message))
 
+// schemas
+const schemaParticipants = Joi.object({ name: Joi.string().required() })
+const schemaMessage = Joi.object({
+    from: Joi.string().required(),
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().required()
+})
+
+
+// primeiro post não concluído (endpoints)
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+
+    const validation = schemaParticipants.validate(req.body)
+
+    if (validation) {
+        return res.sendStatus(422)
+    }
+
+    try {
+        const participantExists = await db.collection("participants").findOne({ name: name })
+        if (participantExists) {
+            return res.sendStatus(409)
+        }
+
+
+        const timestamp = Date.now()
+        await db.collection("participants").insertOne({ name, lastStatus: timestamp })
+
+
+        const message = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(timestamp).format('HH:mm:ss')
+        }
+
+        await db.collection("messages").insertOne(message)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+    res.sendStatus(201)
+})
 
 // Ligar a aplicação do servidor para ouvir requisições
 const PORT = 5000
